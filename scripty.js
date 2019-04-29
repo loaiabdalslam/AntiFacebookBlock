06a9bbc202886f8d5e173be68467a52e82f3dbca
loaiabdalslam <loaiabdalslam@gmail.com>
2019-04-29 21:23:30 +0200

    Ramadan Careem v1 -- not automated Script

--- scripty.js
+++ scripty.js
@@ -2,7 +2,7 @@
 // Get all Blocked people unblockButton and click on it then popup will appear just run the first part 
 // and wait then  you can run  script number (2) :
 console.log('OMG you have blocked',document.getElementsByClassName('uiLinkButtonInput').length)
-for (let i = 0 ; i <= document.getElementsByClassName('uiLinkButtonInput').length; i++){
+for (let i = 0 ; i < document.getElementsByClassName('uiLinkButtonInput').length; i++){
 
 document.getElementsByClassName('uiLinkButtonInput')[i].click()
 console.log('Person Number :',i)
@@ -14,7 +14,7 @@ console.log("Let's clean up that mess")
 // script number 2 : Run it after firstone is okay and you see Let's clean up that mess in the console
 
 if(document.getElementsByClassName('_42ft _4jy0 layerConfirm uiOverlayButton _4jy3 _4jy1 selected _51sy')){
-for (let i = 0 ; i <= document.getElementsByClassName('_42ft _4jy0 layerConfirm uiOverlayButton _4jy3 _4jy1 selected _51sy').length; i++){
+for (let i = 0 ; i < document.getElementsByClassName('_42ft _4jy0 layerConfirm uiOverlayButton _4jy3 _4jy1 selected _51sy').length; i++){
 setTimeout(()=>{document.getElementsByClassName('_42ft _4jy0 layerConfirm uiOverlayButton _4jy3 _4jy1 selected _51sy')
 [i].click()},3000)}}
 
